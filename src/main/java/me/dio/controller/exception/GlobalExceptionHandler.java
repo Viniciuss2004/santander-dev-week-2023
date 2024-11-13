c9831e316781fa1ff2ae7e9bd1ef5d7b1ef3d0ae
Viniciuss2004 <viandcustodio@gmail.com>
2024-11-13 13:03:35 -0300

    Tentando conexão com railway

---
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package me.dio.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handlebusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handlenotFoundException(NoSuchElementException notFoundException) {
+        var message = "Resource id not found.";
+        logger.error("", notFoundException);
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable UnexpectedException) {
+        var message = "Unexpected server error, see the logs.";
+        logger.error("", UnexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
